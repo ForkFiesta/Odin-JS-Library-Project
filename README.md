f515971aad3db9e7241dd8af32bf1b65a239cd74
Raul Resendez <raul.resendez@proton.me>
2024-10-05 02:23:55 -0500

    Restart HTML and CSS. Moving forward section by section.

---
+++ README.md
@@ -0,0 +1,2 @@
+<a href="https://www.flaticon.com/free-icons/search" title="search icons">Search icons created by Catalin Fertu - Flaticon</a>
+
--- javascript.js
+++ javascript.js
@@ -1,12 +1,14 @@
 const myLibrary = []
 
-
+const libraryList = document.querySelector(".library");
 function Book(title, pages, author, read){
     this.title = title;
     this.pages = pages;
     this.author = author;
     this.read = read;
     addBookToLibrary(this);
+    readOutLibrary();
+
 
 }
 
@@ -15,19 +17,25 @@ const addBookToLibrary = ()=>{
 }
 
 const readOutLibrary = ()=>{
+    
+    console.log("we made this far");
     myLibrary.forEach(book => {
-        
+        console.log(book.title);
+    const newBook = document.createElement("li");
+    newBook.innerHTML = `This is a test`;
+    libraryList.appendChild(newBook);
     });
 }
 
 
 const submitButton = document.querySelector('.submit-button');
 // console.log(submitButton);
-submitButton.addEventListener('click', () =>{
+submitButton.addEventListener('click', (event) =>{
     const bookTitle = document.querySelector("#title");
     const bookAuthor = document.querySelector("#author");
     const bookPages = document.querySelector("#pages");
     const hasRead = document.querySelector("input[name=radio]:checked");
+    event.preventDefault();
 
     Book(bookTitle.value,bookAuthor.value,bookPages.value,hasRead);
 
